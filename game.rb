293da5fc3036118d3950cb9ce3776d7b37c2d45d
Juan Couso <juan.couso@gmail.com>
2017-12-16 16:51:01 -0200

    added apple class and collition with apple, TODO adjust better the collition

--- game.rb
+++ game.rb
@@ -7,16 +7,29 @@ class MyGame < Gosu::Window
 
     @background_image = Gosu::Image.new("media/background.jpg", tileable: true)
     @hero = Hero.new
-    @hero.wrap(250, 250)
+    @hero.wrap(100, 250)
+    @apples = []
+    5.times {
+      x = rand(100..500)
+      y = rand(100..350)
+      @apples << Apple.new(x,y)
+    }
   end
 
   def update
     @hero.update
+    #puts @apples[0]
+    @apples.each do |apple|
+      if @hero.got_apple?(apple)
+        @apples.delete(apple)
+      end
+    end
   end
 
   def draw
+    @apples.each { |apple| apple.draw }
     @hero.draw
-    @background_image.draw(0, 0, 0, width = 0.7, heigth = 0.7)
+    @background_image.draw(0, 0, 0, 0.7, 0.7)
   end
 
   def button_down(id)
@@ -25,6 +38,20 @@ class MyGame < Gosu::Window
     else
       super
     end
+  end
+end
+
+
+class Apple
+  attr_reader :x, :y
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @apple = Gosu::Image.new("media/apple.png")
+  end
+
+  def draw
+    @apple.draw(@x, @y, 1, 0.1, 0.1)
   end
 end
 
@@ -49,7 +76,7 @@ class Hero
 
     if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
       if @jump == false
-        @vel_y = -20
+        @vel_y = -15
         @jump = true
       end
     end
@@ -77,12 +104,12 @@ class Hero
     @x += 6
   end
 
-  def move_up
-    @y += 1
+  def got_apple?(apple)
+    (apple.x > @x) && (apple.y > @y)
   end
 
   def draw
-    @image.draw(@x, @y, 1, scale_y = 0.3, scale_x = 0.3)
+    @image.draw(@x, @y, 2, scale_y = 0.3, scale_x = 0.3)
   end
 end
 
